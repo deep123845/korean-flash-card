68eef90864ffcac579abb287578149e112907f60
Deep123845 <deep123845@gmail.com>
2026-06-02 19:09:46 -0400

    added basics for sino number page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import TabLabel from './components/tab-label';
+import SinoPage from './pages/sino-page';
 
 function App() {
   const [tab, setTab] = useState("sino");
@@ -11,6 +12,9 @@ function App() {
         <TabLabel label='Sino Numbers' onClick={() => { setTab("sino") }} selected={tab == "sino"} />
         <TabLabel label='Native Numbers' onClick={() => { setTab("native") }} selected={tab == "native"} />
         <TabLabel label='Clock Time' onClick={() => { setTab("clock") }} selected={tab == "clock"} />
+      </div>
+      <div>
+        {tab == "sino" && <SinoPage />}
       </div>
     </>
   )
---
+++ src/pages/sino-page.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+
+const MAX_NUMBER = 9999;
+
+const digitTranslations: Record<string, string> = {
+    "1": "일",
+    "2": "이",
+    "3": "삼",
+    "4": "사",
+    "5": "오",
+    "6": "육",
+    "7": "칠",
+    "8": "팔",
+    "9": "구",
+}
+
+const placeTranslations: Record<string, string> = {
+    "1": "십",
+    "2": "백",
+    "3": "천",
+}
+
+function translate(number: number): string {
+    let translatedList = [];
+    const digits = number.toString().split("").reverse();
+    for (const index in digits) {
+        if (index != "0") {
+            translatedList.push(placeTranslations[index]);
+        }
+
+        const digit = digits[index];
+        if (digit != "0") {
+            translatedList.push(digitTranslations[digit]);
+        }
+    }
+    const translatedNumber = translatedList.reverse().join(" ");
+    return translatedNumber;
+}
+
+export default function SinoPage() {
+    const [number, setNumber] = useState(0);
+    const [numberVisible, setNumberVisible] = useState(false);
+    const [flip, setFlip] = useState(false);
+    const translatedNumber = translate(number);
+
+    function generateRandomNumber() {
+        setNumber(Math.floor(Math.random() * (MAX_NUMBER + 1)));
+    }
+
+    useEffect(() => {
+        generateRandomNumber();
+    }, [])
+
+    return (
+        <>
+            <div className="grid grid-cols-1 m-5 gap-5 text-5xl justify-items-center">
+                <div className="border-1 p-3 pt-2 w-fit rounded-xl" onClick={() => { setFlip(!flip) }}>Flip</div>
+                <div className="border-1 p-3 pt-2 w-fit rounded-xl" onClick={() => { generateRandomNumber() }}>Random</div>
+                <div className="border-1 p-3 pt-2 w-fit rounded-xl" onClick={() => setNumberVisible(!numberVisible)}>Show Translation</div>
+                <div className={"flex gap-5 " + (flip ? "flex-col-reverse" : "flex-col")}>
+                    <div>{!flip ? translatedNumber : (numberVisible ? translatedNumber : "***")}</div>
+                    <div>{flip ? number : (numberVisible ? number : "***")}</div>
+                </div>
+            </div>
+        </>
+    )
+}
